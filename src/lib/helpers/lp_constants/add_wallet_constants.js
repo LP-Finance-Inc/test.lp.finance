e4876e58e28094cad4e7b99b014f26278c72fa1f
monkeyMax-SVG <monkeymax@yeah.net>
2022-04-25 11:35:25 +0800

    resize whitelist

--- src/lib/helpers/lp_constants/add_wallet_constants.js
+++ src/lib/helpers/lp_constants/add_wallet_constants.js
@@ -2,8 +2,8 @@ import * as anchor from "@project-serum/anchor";
 const { PublicKey } = anchor.web3;
 
 export const whiteListKey = new PublicKey(
-  "6QDWT8jYDCTx5zrpqNmtm3SpHUZuEn8wVXW7WESqmpd9"
+  "AyGQrdBpRmdFtHES54vyD3cZUy7j61a9Zi4esRVRa2xd"
 );
 export const configAccountKey = new PublicKey(
-  "9EzbCTagJ1Xxfb5T5HLu1voN5HcMN6SfeCyod2KhAvgG"
+  "J4pZUiENgJheRLogmbdeyfUvk46dAwyyjXPpW9HGbVm3"
 );
