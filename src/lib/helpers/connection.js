7831445cd72ee7fad9e81979d30936c778dc0614
Joohwan Lee <aperire0402@gmail.com>
2022-05-04 12:17:44 -0400

    Remove devnet quicknode

--- src/lib/helpers/connection.js
+++ src/lib/helpers/connection.js
@@ -1,7 +1,7 @@
 import * as anchor from "@project-serum/anchor";
 const { Connection } = anchor.web3;
 
-const NETWORK = "https://lively-silent-waterfall.solana-devnet.quiknode.pro/9c293b47cf73c7c49e0f760fabbc803b94b5c10a/";
+const NETWORK = "https://api.devnet.solana.com";
 
 const NETWORK_MAINNET = "https://young-black-sun.solana-mainnet.quiknode.pro/";
 
