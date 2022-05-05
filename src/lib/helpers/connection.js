f0ecd5e7fbfbb8be704e9dd39a794188a19dac7b
Joohwan Lee <aperire0402@gmail.com>
2022-05-05 04:31:34 -0400

    Update connection.js
    Change endpoint to quicknode

--- src/lib/helpers/connection.js
+++ src/lib/helpers/connection.js
@@ -1,9 +1,9 @@
 import * as anchor from "@project-serum/anchor";
 const { Connection } = anchor.web3;
 
-const NETWORK = "https://api.devnet.solana.com";
+const NETWORK = "https://fragrant-dry-butterfly.solana-devnet.quiknode.pro/16d1c9ded067bd503dc8e393355d89e38e61834f/";
 
-const NETWORK_MAINNET = "https://young-black-sun.solana-mainnet.quiknode.pro/";
+const NETWORK_MAINNET = "https://withered-cool-brook.solana-mainnet.quiknode.pro/b44d1222c014690f954ef6bce03952c76dbfc691/";
 
 const netConfig = "devnet";
 
