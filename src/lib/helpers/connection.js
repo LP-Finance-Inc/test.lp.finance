50311080ded8cb6364bfd296361f31b60d33ad98
monkeyMax-SVG <monkeymax@yeah.net>
2022-05-05 16:35:21 +0800

    updated devnet rpc

--- src/lib/helpers/connection.js
+++ src/lib/helpers/connection.js
@@ -1,7 +1,7 @@
 import * as anchor from "@project-serum/anchor";
 const { Connection } = anchor.web3;
 
-const NETWORK = "https://api.devnet.solana.com";
+const NETWORK = "https://fragrant-dry-butterfly.solana-devnet.quiknode.pro/16d1c9ded067bd503dc8e393355d89e38e61834f/";
 
 const NETWORK_MAINNET = "https://young-black-sun.solana-mainnet.quiknode.pro/";
 
