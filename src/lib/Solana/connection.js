ec53a63bf4265cd331c354b2ecc048c94fcfdd7c
Deepakkurmi-eMoment <deepak.kurmi@emoment.in>
2022-07-27 20:41:35 +0530

    changed rpc

--- src/lib/Solana/connection.js
+++ src/lib/Solana/connection.js
@@ -1,13 +1,13 @@
 import * as anchor from "@project-serum/anchor";
 const { Connection } = anchor.web3;
 
-const NETWORK = "https://fragrant-dry-butterfly.solana-devnet.quiknode.pro/16d1c9ded067bd503dc8e393355d89e38e61834f/";
+const NETWORK = "https://api.devnet.solana.com"; // "https://api.devnet.solana.com"; //
 
-const NETWORK_MAINNET = "https://withered-cool-brook.solana-mainnet.quiknode.pro/b44d1222c014690f954ef6bce03952c76dbfc691/";
+const NETWORK_MAINNET = "https://solana-api.projectserum.com/";
 
 const netConfig = "devnet";
 
 const connection = new Connection(NETWORK, "processed");
 const MainnetConnection = new Connection(NETWORK_MAINNET, "processed");
 
-export { connection, NETWORK, netConfig, NETWORK_MAINNET,MainnetConnection };
+export { connection, NETWORK, netConfig, NETWORK_MAINNET, MainnetConnection };
